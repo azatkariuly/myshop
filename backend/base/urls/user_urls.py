232e4bd073d2e5b6eeb3ab55a32038ccfcf23037
azatkariuly <azat.kariuly@gmail.com>
2023-02-23 22:34:57 +0900

    added frontend user authentication

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from base.views import user_views as views
 
 urlpatterns = [
-    path('login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
-    path('profile', views.getUserProfile, name='user_profile'),
+    path('profile/', views.getUserProfile, name='user_profile'),
+    path('profile/update/', views.updateUserProfile, name='user_profile_update'),
     path('', views.getUsers, name='users'),
 ]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen/>} exact />
+            <Route path='/login' element={<LoginScreen/>} />
+            <Route path='/register' element={<RegisterScreen/>} />
+            <Route path='/profile' element={<ProfileScreen/>} />
             <Route path='/product/:id' element={<ProductScreen/>} />
             <Route path='/cart/:id?' element={<CartScreen/>} />
           </Routes>
